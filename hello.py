5f5ec7a9833c14fb5c1645a065e0e4425700c640
Florian Dambrine <florian@gumgum.com>
2018-11-29 09:08:33 -0800

    Update app

--- hello.py
+++ hello.py
@@ -3,9 +3,12 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return render_template("index.html")
+@app.route("/<name>")
+def hello(name="World"):
+    return render_template("index.html", name=name)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
